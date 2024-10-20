f8e02144e1ac67eb54cce0568a8ffed1e9cc5249
Daniar Makyev <daniarmakyev10@gmail.com>
2024-10-20 21:36:07 +0600

    fix old shit

--- src/MainRoutes/MainRoutes.jsx
+++ src/MainRoutes/MainRoutes.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home/Home'
 import MainLayut from '../MainLayout/MainLayut'
+import Search from '../components/Search/Search'
+import Explore from '../components/Explore/Explore'
 
 
 const MainRoutes = () => {
@@ -9,6 +11,8 @@ const MainRoutes = () => {
     <Routes>
         <Route path="/" element={<MainLayut/>}>
             <Route path='/' element={<Home/>}/>
+            <Route path='/search' element={<Search/>}/>
+            <Route path='/explore' element={<Explore/>}/>
         </Route>
     </Routes>
   )
